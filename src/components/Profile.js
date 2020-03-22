dc3e3e25fc6a24ca00b249ec5fb94e3ec6451bd9
Brontesewell <brontsewell7@gmail.com>
2020-03-21 18:07:29 -0700

    profile starting styling boxes

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -4,32 +4,35 @@ import Navbar from '../containers/Navbar'
 import {Link} from 'react-router-dom'
 import Children from './Children'
 import CourseCollection from './CourseCollection'
-
+import Footer from '../containers/Footer'
 
 const Profile = (props) => {
 console.log(props.currentUser.kids)
     const {first_name, last_name, email} = props.currentUser
 
     return (
-        <div>
+        <div >
             <div className = 'row'>
                {<Navbar/>} 
             </div>
-            <div className = 'row'>
-            <h1 className="Catagory">My Family</h1>
-             <div className="line-catagory"></div>
+            <div id="profiles">
+            <div className = "my-fam">
+            <h1 className="Catagory">My Profile</h1>
+             <div className="line-profile"></div>
                 <h3 className="p">{first_name + ' ' + last_name}</h3>
                 <h6><strong>Email: </strong>{email}</h6>
-               </div>
-               <div className = 'row '>
+              
                 <div >
-                    <Link to='/edit-profile'><button id="sign-in-button" className="btn">Edit Profile</button></Link>
+                    <Link to='/edit-profile'><button id="edit-p" className="btn">Edit Profile</button></Link>
                     </div>
             </div>
 
+
+
+
             <div id = "boxes"> 
             <div id = "leftbox"> 
-                <h3>Children</h3>  
+                <h3 id="my-children"> My Children</h3>  
                         <Link to='/add-child'><button id="sign-in-button" className="btn">Add another Child</button></Link>
                
                 <br></br>
@@ -46,6 +49,8 @@ console.log(props.currentUser.kids)
               
           
         </div> 
+
+        </div>
         </div>
     )
 }
