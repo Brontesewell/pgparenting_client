01e529fa1f49a54be27700afb912adc2e7bed443
Brontesewell <brontsewell7@gmail.com>
2020-03-24 15:33:01 -0700

    journals box

---
+++ src/components/JournalList.js
@@ -0,0 +1,27 @@
+
+import React, { Component } from 'react';
+import Navbar from '../containers/Navbar'
+import {connect} from 'react-redux'
+import addKid from '../actions/addKid'
+
+
+class JournalList extends Component {
+
+    
+    render() {
+      
+
+        return (
+            <div>
+                
+              <div id="journal-buble">
+              <h1 id="journal-text">{this.props.journal.text}</h1>
+                <h5 id="created-j"><i>Created at {this.props.journal.date_now}</i></h5>
+              </div> 
+            </div>
+        );
+    }
+}
+
+
+export default JournalList;
--- src/components/JournalsPage.js
+++ src/components/JournalsPage.js
@@ -3,21 +3,23 @@ import React, { Component } from 'react';
 import Navbar from '../containers/Navbar'
 import {connect} from 'react-redux'
 import addKid from '../actions/addKid'
-
+import JournalList from './JournalList'
 
 class JournalsPage extends Component {
 
     
     render() {
-        
+        console.log(this.props.location.state.clickedjournal.journals)
 
         return (
             <div>
                 <div className = 'row'>
                     {<Navbar/>}
                 </div>
-              <h1>hello</h1>
-        <h1>{this.props.location.state.clickedjournal.name}</h1>
+              <h1 id="title-journal">{this.props.location.state.clickedjournal.name}'s  Journals</h1>
+              <div id="line-journal-title"></div>
+
+        <h1>{this.props.location.state.clickedjournal.journals.map(journal => < JournalList journal={journal} />)}</h1>
         <button id="course-button-back" className="btn" onClick={() => this.history.push('/profile')}>Back</button>
             </div>
         );
