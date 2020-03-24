d528fed05e223e5490c31b05bd93c478149ae2c2
Brontesewell <brontsewell7@gmail.com>
2020-03-24 11:11:39 -0700

    progress

--- src/containers/AddKid.js
+++ src/containers/AddKid.js
@@ -33,7 +33,7 @@ class AddKid extends Component {
     render() {
         
 
-        const {name, birthday, gender, behaviour_progress, user_id} = this.state
+        const {name, birthday, gender, behaviour_progress, behaviour_score, sport_progress, sport_score, academic_progress, academic_score, character_progress, character_score, user_id} = this.state
         const {history} = this.props
 
         return (
@@ -42,7 +42,9 @@ class AddKid extends Component {
                     {<Navbar/>}
                 </div>
                 <div className = 'form-edit'>
+                    <h2>Add a Child</h2>
                     <form className = 'container' onSubmit={(e) => this.props.addKid(e, this.state, history)}>
+                       <div id="add-kid-general">
                         <label htmlFor="name">Name</label>
                         <input name="name" value={name} onChange={this.handleChange}/>
 
@@ -57,40 +59,213 @@ class AddKid extends Component {
 
                        <label htmlFor="birthday">Birthday</label>
                         <input type='text' name="birthday" value={birthday} onChange={this.handleChange}/>
+                        </div>
 
-<h4>Progress</h4>
-<h6> 1 = Getting Worse</h6>
-<h6> 2 = No Progress</h6>
-<h6> 3 = Progress</h6>
-<h6> 4 = Very Good Progress</h6>
-<br></br>
-    <label htmlFor="Behaviour Progress">Behaviour Progress</label>  
-    <br></br>   
-      <label>
-        <input name="behaviour_progress" value="1" type="radio" checked onChange={this.handleChange} checked={behaviour_progress === '1'}/>
-        <span>1</span>
-      </label>
+                        <div id="progress">
+                        <h4>Progress</h4>
+                        <h6> 1 = Getting Worse</h6>
+                        <h6> 2 = No Progress</h6>
+                        <h6> 3 = Progress</h6>
+                        <h6> 4 = Very Good Progress</h6>
+                        <br></br>
+
+                            <label htmlFor="Behaviour Progress">Behaviour Progress</label>  
+                            <br></br>   
+                              <label>
+                                <input name="behaviour_progress" value="1" type="radio" checked onChange={this.handleChange} checked={behaviour_progress === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="behaviour_progress" value="2" type="radio" onChange={this.handleChange} checked={behaviour_progress === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="behaviour_progress" value="3" type="radio" onChange={this.handleChange} checked={behaviour_progress === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="behaviour_progress" value="4" type="radio" onChange={this.handleChange} checked={behaviour_progress === '4'}/>
+                                <span>4</span>
+                              </label>
     
-      <label>
-        <input name="behaviour_progress" value="2" type="radio" onChange={this.handleChange} checked={behaviour_progress === '2'}/>
-        <span>2</span>
-      </label>
-      <label>
-        <input name="behaviour_progress" value="3" type="radio" onChange={this.handleChange} checked={behaviour_progress === '3'}/>
-        <span>3</span>
-      </label>
-   
-      <label>
-        <input name="behaviour_progress" value="4" type="radio" onChange={this.handleChange} checked={behaviour_progress === '4'}/>
-        <span>4</span>
-      </label>
+                              <br></br> 
+                              <br></br> 
+
+                              <label htmlFor="Sport Progress">Sport Progress</label>  
+                            <br></br>   
+                              <label>
+                                <input name="sport_progress" value="1" type="radio" checked onChange={this.handleChange} checked={sport_progress === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="sport_progress" value="2" type="radio" onChange={this.handleChange} checked={sport_progress === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="sport_progress" value="3" type="radio" onChange={this.handleChange} checked={sport_progress === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="sport_progress" value="4" type="radio" onChange={this.handleChange} checked={sport_progress === '4'}/>
+                                <span>4</span>
+                              </label>
+
+                              <br></br> 
+                              <br></br> 
+
+                              <label htmlFor="Academic Progress">Academic Progress</label>  
+                            <br></br>   
+                              <label>
+                                <input name="academic_progress" value="1" type="radio" checked onChange={this.handleChange} checked={academic_progress === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="academic_progress" value="2" type="radio" onChange={this.handleChange} checked={academic_progress === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="academic_progress" value="3" type="radio" onChange={this.handleChange} checked={academic_progress === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="academic_progress" value="4" type="radio" onChange={this.handleChange} checked={academic_progress === '4'}/>
+                                <span>4</span>
+                              </label>
+<br></br> 
+<br></br> 
+
+
+                              <label htmlFor="Character Progress">Character Progress</label>  
+                            <br></br>   
+                              <label>
+                                <input name="character_progress" value="1" type="radio" checked onChange={this.handleChange} checked={character_progress === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="character_progress" value="2" type="radio" onChange={this.handleChange} checked={character_progress === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="character_progress" value="3" type="radio" onChange={this.handleChange} checked={character_progress === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="character_progress" value="4" type="radio" onChange={this.handleChange} checked={character_progress === '4'}/>
+                                <span>4</span>
+                              </label>
+                              </div>
+
+                                
+                    
+                    <div id="score-add-kid">
+                                <h4>Score</h4>
+                                  <h6> 1 = Very Bad</h6>
+                                  <h6> 2 = Poor</h6>
+                                   <h6> 3 = Good</h6>
+                                   <h6> 4 = Very Good</h6>
+                               <br></br>
+
+                               <label htmlFor="Behaviour Score">Behaviour Score</label>  
+                            <br></br>   
+                              <label>
+                                <input name="behaviour_score" value="1" type="radio" checked onChange={this.handleChange} checked={behaviour_score === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="behaviour_score" value="2" type="radio" onChange={this.handleChange} checked={behaviour_score === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="behaviour_score" value="3" type="radio" onChange={this.handleChange} checked={behaviour_score === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="behaviour_score" value="4" type="radio" onChange={this.handleChange} checked={behaviour_score === '4'}/>
+                                <span>4</span>
+                              </label>
     
-  
+                              <br></br> 
+                              <br></br> 
 
-                      
+                              <label htmlFor="Sport Score">Sport Score</label>  
+                            <br></br>   
+                              <label>
+                                <input name="sport_score" value="1" type="radio" checked onChange={this.handleChange} checked={sport_score === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="sport_score" value="2" type="radio" onChange={this.handleChange} checked={sport_score === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="sport_score" value="3" type="radio" onChange={this.handleChange} checked={sport_score === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="sport_score" value="4" type="radio" onChange={this.handleChange} checked={sport_score === '4'}/>
+                                <span>4</span>
+                              </label>
+
+                              <br></br> 
+                              <br></br> 
+
+                              <label htmlFor="Academic Score">Academic Score</label>  
+                            <br></br>   
+                              <label>
+                                <input name="academic_score" value="1" type="radio" checked onChange={this.handleChange} checked={academic_score === '1'}/>
+                                <span>1</span>
+                              </label>
+
+                              <label>
+                                <input name="academic_score" value="2" type="radio" onChange={this.handleChange} checked={academic_score === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="academic_score" value="3" type="radio" onChange={this.handleChange} checked={academic_score === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="academic_score" value="4" type="radio" onChange={this.handleChange} checked={academic_score === '4'}/>
+                                <span>4</span>
+                              </label>
+<br></br> 
+<br></br> 
 
 
+                              <label htmlFor="Character Score">Character Score</label>  
+                            <br></br>   
+                              <label>
+                                <input name="character_score" value="1" type="radio" checked onChange={this.handleChange} checked={character_score === '1'}/>
+                                <span>1</span>
+                              </label>
 
+                              <label>
+                                <input name="character_score" value="2" type="radio" onChange={this.handleChange} checked={character_score === '2'}/>
+                                <span>2</span>
+                              </label>
+                              <label>
+                                <input name="character_score" value="3" type="radio" onChange={this.handleChange} checked={character_score === '3'}/>
+                                <span>3</span>
+                              </label>
+
+                              <label>
+                                <input name="character_score" value="4" type="radio" onChange={this.handleChange} checked={character_score === '4'}/>
+                                <span>4</span>
+                              </label>
+                              </div>
 
                         <br></br>
                         <br></br>
