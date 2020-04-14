8f86b74fc06e4aa55c9d2db4a60b34f4bb8f51a4
Brontesewell <brontsewell7@gmail.com>
2020-04-13 17:11:05 -0700

    nav bar added parenting tips

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -11,31 +11,17 @@ class Navbar extends Component {
     container = React.createRef();
     state = {
       firstName: this.props.currentUser.first_name,
+      button: "☰",
     };
 
-    // componentDidMount() {
-    //   document.addEventListener("mousedown", this.handleClickOutside);
-    // }
 
-    // componentWillUnmount() {
-    //   document.removeEventListener("mousedown", this.handleClickOutside);
-    // }
+    clickButton = () => {
+      this.setState({
+        button: "X"
+      })
+    }
 
-    // handleClickOutside = event => {
-    //   if (this.container.current && !this.container.current.contains(event.target)) {
-    //     this.setState({
-    //       open: false,
-    //     });
-    //   }
-    // };
 
-    // handleButtonClick = () => {
-    //   this.setState(state => {
-    //     return {
-    //       open: !state.open,
-    //     };
-    //   });
-    // };
 
     handleSignOut = e => {
         localStorage.clear()
@@ -56,7 +42,7 @@ class Navbar extends Component {
                   
         <div class="menu-item-nav">
                     
-                    <button type="button" id="menu-item-nav-threelines" class="button">☰</button>
+                    <button type="button" id="menu-item-nav-threelines" class="button">{this.state.button}</button>
                     <div class="container">
                    <ul>
           
@@ -75,37 +61,31 @@ class Navbar extends Component {
               
       
         
+              </div> 
               
-       
-                    {/* <div className="container-nav" id="overlay" ref={this.container}>
-                      <button type="button" class="button" onClick={this.handleButtonClick}>
-                        ☰
-                      </button>
-                      {this.state.open && (
-                        <div class="container">
-                          <ul>
-                      <br></br>
-                      <br></br>
-                            <li><Link to = '/home'>Home</Link></li>
-                            <li>Our Mission</li>
-                            <li><Link to = '/profile' >My Family</Link></li>
-                            <li>Chatroom</li>
-                            <li>Shop</li>
-                            <li>Contact Us</li>
-                            <li><Link to = '/' onClick={this.handleSignOut}>Sign Out</Link></li>
-                          </ul>
-                        </div>
-                      )}
-                    </div> */}
+                   <div class="menu-item">
+                          <h6 onClick={() =>  window.location.href='/co-parents'}   id="menu-item-p">Co-Parenting</h6>
                  
-                  
-                </div> 
+                          </div>
+       
                 
-      
+        <div class="menu-item-tips">
+              <h6 id="menu-item-p">Parenting Tips</h6>
+                   <ul>
+          
+                   <li className="parent-lis"><a href="/baby">0-12mths</a></li>
+                       <li className="parent-lis"><a href="/toddler">1-2yrs</a></li>
+                        <li className="parent-lis"><a href="/pre_school"> 3-5yrs</a></li>
+                        <li className="parent-lis"><a href="/primary_school"> 6-10yrs</a></li>
+                         <li className="parent-lis"><a href="/preteen">11-12yrs</a></li>
+                        <li  className="parent-lis"><a href="/teen">13-18yrs</a></li>
+                        <li className="parent-lis"><a href="/young_adults">19-25yrs</a></li>
+                   </ul>
+        </div>
         
               
        <div class="menu-item">
-                <h5 id="menu-item-p">Parenting Courses</h5>
+                <h6 id="menu-item-p">Parenting Courses</h6>
                    <ul>
                    
                         <li className="parent-lis"><a href="/baby">0-12mths</a></li>
@@ -119,25 +99,6 @@ class Navbar extends Component {
                    </ul>
         </div>
 
-        {/* <div class="menu-item">
-      <Link to = '/parenting-tips' className="parenting-links">Parenting Tips</Link>
-                   <ul>
-          
-                        <li><a href="/baby">0-12mths</a></li>
-                       <li><a href="/toddler">1-2yrs</a></li>
-                        <li><a href="/pre_school"> 3-5yrs</a></li>
-                        <li><a href="/primary_school"> 6-10yrs</a></li>
-                         <li><a href="/preteen">11-12yrs</a></li>
-                        <li><a href="/teen">13-18yrs</a></li>
-                        <li><a href="/young_adults">19-25yrs</a></li>
-                   </ul>
-
-
-        </div> */}
-        <div class="menu-item-co-parent">
-               <h5 onClick={() =>  window.location.href='/co-parents'}   id="menu-item-p">Co-Parenting</h5>
-      
-               </div>
        
         <br></br>
    
