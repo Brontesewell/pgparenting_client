edc08db333cef4a8937aa64f59902e4de17825e8
Brontesewell <brontsewell7@gmail.com>
2020-05-22 20:53:06 -0700

    collapse navbar when browser shrinks in welcome

--- src/containers/WelcomeNav.js
+++ src/containers/WelcomeNav.js
@@ -1,44 +1,29 @@
-import {FormControl, Nav, Button, Navbar, Form} from 'react-bootstrap'
+import {FormControl, Nav, Button, Navbar, Form, NavDropdown} from 'react-bootstrap'
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../Logo.jpg'
 
 
-const WelcomeNav = props =>  {
-    const { location } = props;
-    // render() {
+const WelcomeNav = () =>  {
         return (
             <nav id="wecome-navbar">
 
-                {/* <div className="container">
-                 
-                    <a href="/"><img className ='logo' alt="Logo" src={Logo}/></a>
-
-                    <Link to='/contact-us' className="nav-links" >Contact Us</Link>
-                      <Link to='/signup-user' className="nav-links" >Join Now</Link>
-                        <Link to='/signin-user' className="nav-links">Login</Link>
-                
-      
-                    </div> */}
                     
-
-                    <>
-
-                    <Navbar bg="light" variant="light">
-                        <Navbar.Brand href="/"><img  alt="Logo" src={Logo}/></Navbar.Brand>
-                        <Nav className="ml-auto" activeKey={location}>
+                    <Navbar bg="light" expand="lg">
+                    <Navbar.Brand href="/"><img  alt="Logo" src={Logo}/></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto">
                         <Nav.Link className="nav-links" href="/signin-user">Login</Nav.Link>
                         <Nav.Link  className="nav-links" href="/signup-user">Join Now</Nav.Link>
                         <Nav.Link  className="nav-links" href="/contact-us">Contact Us</Nav.Link>
                         </Nav>
+                    </Navbar.Collapse>
                     </Navbar>
-                    </>
        
              </nav>
         );
     }
-
-// }
 
 export default WelcomeNav;
 
