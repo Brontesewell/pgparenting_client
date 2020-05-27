d8c3223d27da773fdc12164b4ce63164b4765047
Brontesewell <brontsewell7@gmail.com>
2020-05-27 14:16:15 -0700

    sign up stlying

--- src/containers/SignUpUser.js
+++ src/containers/SignUpUser.js
@@ -4,6 +4,7 @@ import signUpUser from '../actions/signUpUser'
 import Logo from '../Logo.jpg'
 import { Link } from "react-router-dom";
 import WelcomeNav from './WelcomeNav'
+import { MDBContainer, MDBRow, MDBCol, MDBBtn } from 'mdbreact';
 
 
 export class SignUpUser extends Component {
@@ -22,13 +23,6 @@ export class SignUpUser extends Component {
             [name]: value
         })
     }
-    
-    // handleSubscribe = (e) => {
-    //     // const {subscribe, value} = e.target
-    //     this.setState({
-    //         subscribe: true
-    //     })
-    // }
 
     render() {
        const { firstName, lastName, email, password, subscribe } = this.state
@@ -36,50 +30,62 @@ export class SignUpUser extends Component {
         return (
            <div>
                 <WelcomeNav />
-            <div className="container-signup">
-                <h4 id="sign_in">Sign Up</h4>
-                
-                 {/* <a href="/"><img className ='logo-signin' alt="Logo" src={Logo}/></a> */}
-              
-               
-                <form onSubmit={(e) => this.props.signUpUser(e, this.state, history)}>
+                <div id="login-div">
+
+                    <MDBContainer className="container-signup">
+                    <MDBCol md="5">
+                         <form onSubmit={(e) => this.props.signUpUser(e, this.state, history)} >
+
+                            <p className="login-title text-center mb-4">PG <span id="parenting">Parenting</span> Sign Up</p>
+                            <div class="container">
                                 <div class="row">
-                                   <div class="input-field col s6">
-                                   <label>First Name</label>
-                                    <input type="text" name="firstName" placeholder="First name" value={firstName} onChange={this.handleChange}/>
-                                   </div>
-
-                                  <div class="input-field col s6">
-                                  <label>Last Name</label>
-                                <input type="text" name="lastName" placeholder="Last name" value={lastName} onChange={this.handleChange}/>
-
-                                  </div>
+                                    <div class="col-lg fname-forms">
+                                        <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                                            First Name*
+                                        </label>
+                                        <input type="text" name="firstName" placeholder="First name" value={firstName} onChange={this.handleChange} id="defaultFormLoginEmailEx" className="form-control" />       
+                                    </div>
+                                    <div class="col-lg lname-forms">
+                                        <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                                            Last Name*
+                                        </label>
+                                            <input type="text" name="lastName" placeholder="Last name" value={lastName} onChange={this.handleChange} id="defaultFormLoginPasswordEx" className="form-control" />
+                                    </div>
+                                </div>
+                             </div>
                         
-                   
-                       <div className="email-password">
-                        <label>Email</label>
-                        <input type='email' name="email" placeholder="Email" value={email} onChange={this.handleChange}/>
-                    
-                    
-                        <label>Password</label>
-                        <input type='password' name="password" placeholder="Password" value={password} onChange={this.handleChange}/>
-                        
-                        </div>
-                        
-                                  </div>
-                        <h5>Email Preference</h5>
-                        <label>
-                        <input type="checkbox" name="subscribe" class="filled-in" onChange={this.handleChange} value="true" checked={subscribe === 'true'}/>
-                         <span>PG Parenting Newsletter</span>
-                        </label>
+                            <label htmlFor="defaultFormLoginEmailEx" className="grey-text">
+                                Your Email*
+                            </label>
+                            <input type='email' name="email" placeholder="Email" value={email} onChange={this.handleChange} id="defaultFormLoginEmailEx" className="form-control" />
+                            <br></br>
 
-                    <button  id="sign-up-button" className="btn-large" type="submit">Next</button>
-                    <br></br>
+                            <label htmlFor="defaultFormLoginPasswordEx" className="grey-text">
+                                Your Password*
+                            </label>
+                            <input type='password' name="password" placeholder="Password" value={password} onChange={this.handleChange} id="defaultFormLoginPasswordEx" className="form-control" />
+                        
+
+                            <br></br>
+
+                            <h5 style={{fontWeight: 'normal'}}>Email Preference</h5>
+                            <label>
+                            <input type="checkbox" name="subscribe" class="filled-in" onChange={this.handleChange} value="true" checked={subscribe === 'true'}/>
+                            <span>PG Parenting Newsletter</span>
+                            </label>
+                            
+                                <div className="text-center mt-4">
+                                    <button className="btn-login" color="indigo" type="submit">Continue  →</button>
+                                </div>
+                  
                 <Link to='/signin-user' className="links-signup" >Or Login</Link>
                 
                 </form>
+                </MDBCol>
+                
+                </MDBContainer>
 
-            </div>
+                </div>
             </div>
         );
     }
