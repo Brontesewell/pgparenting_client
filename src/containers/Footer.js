e8c0ce2f70801a69696f3998086586ce641e8900
Brontesewell <brontsewell7@gmail.com>
2020-06-04 17:15:48 -0700

    footer changed to bootstrap

--- src/containers/Footer.js
+++ src/containers/Footer.js
@@ -34,56 +34,63 @@ class Footer extends Component {
             <div id="footer">
 <footer class="footer-distributed">
 
-    <a href="/home"><img className ='logo-footer' alt="Logo" src={Logo}/></a>
-    
-    <div class="footer-left">
-        <p class="footer-links">
-            <a href="#" class="link-1">Home</a>
-            
-            <a href="#">Blog</a>
-        
-            <a href="#">About</a>
-            
-            <a href="#">Contact</a>
-        </p>
-
-        <p id="year-footer">PG Parenting © 2020</p>
-    </div>
-
-    <div class="footer-center">
-        <h5 id="contact-footer">Connect with us</h5>
-                        <SocialIcon className="share-button" url="https://www.instagram.com/pgparenting/" style={{ height: 35, width: 35 }}/>
-                        <SocialIcon className="share-button" url="https://www.facebook.com" style={{ height: 35, width: 35 }}/>
-                        <SocialIcon className="share-button" url="https://www.youtube.com" style={{ height: 35, width: 35 }}/>
-                        <p id="email-icon-footer-welcome"><FontAwesomeIcon icon={faEnvelope} id="email-footer" /> pgparenting@gmail.com</p>
-        
-
-
-    </div>
-
-    <div class="footer-right">
-    
-    <h5 id="newsletter-title">Sign up for <strong id="newsletter-logo">PG Parenting</strong> newsletter</h5>
-                        <div class="row" id="newsletter-div">
-                              <form className = 'col s12' onSubmit={(e)=> editProfile(e, history, this.state, currentUser.id)}>
-                                  <div class="row">
-                                   <div class="row">
-                                 
-                                 </div> 
-                                {currentUser.subscribe === "true" ? <button class="btn-newsletter-clicked" disabled>Subscribed ✓</button> :  <div>
-                        <label>
-                        <input type="checkbox" name="subscribe" class="filled-in" onChange={this.handleChange} value="true" checked={subscribe === 'true'}/>
-                         <span>{currentUser.email}</span>
-                        </label>
-                        <br></br>
-                       
+<div class="container">
+                    <div class="row">
+                        <div class="col-lg">
+                            <a href="/home"><img className ='logo-footer' alt="Logo" src={Logo}/></a>
+                                <p class="footer-links">
+                                    <a href="#" class="link-1">Home</a>
+                                    
+                                    <a href="#">Blog</a>
+                                
+                                    <a href="#">About</a>
+                                    
+                                    <a href="#">Contact</a>
+                                </p>
                         
-                        <button className="btn-newsletter" type="submit">Confirm</button>
-                        </div>}
-                          </div>
-                        </form>
-                    </div>  
-    </div>
+                                <p id="year-footer">PG Parenting © 2020</p>
+                        
+                        
+                    
+                        </div>
+                        <div class="col-lg">
+                                 
+
+                            <h5 id="contact-footer">Connect with us</h5>
+                            <SocialIcon className="share-button" url="https://www.instagram.com/pgparenting/" style={{ height: 35, width: 35 }}/>
+                            <SocialIcon className="share-button" url="https://www.facebook.com" style={{ height: 35, width: 35 }}/>
+                            <SocialIcon className="share-button" url="https://www.youtube.com" style={{ height: 35, width: 35 }}/>
+                            <p id="email-icon-footer-welcome"><FontAwesomeIcon icon={faEnvelope} id="email-footer" /> pgparenting@gmail.com</p>
+        
+                        </div>
+                        <div class="col-lg">
+                                 
+                                <h5 id="newsletter-title">Sign up for <strong id="newsletter-logo">PG Parenting</strong> newsletter</h5>
+                                            
+                                                <form className = 'col s12' onSubmit={(e)=> editProfile(e, history, this.state, currentUser.id)}>
+                                                    <div >
+                                                    
+                                                    {currentUser.subscribe === "true" ? <button class="btn-newsletter-clicked" disabled>Subscribed ✓</button> :  
+                                                        <div>
+                                                                <label>
+                                                                <input type="checkbox" name="subscribe" class="filled-in" onChange={this.handleChange} value="true" checked={subscribe === 'true'}/>
+                                                                <span>{currentUser.email}</span>
+                                                                </label>
+                                                                <br></br>
+                                                            
+                                                                
+                                                                <button className="btn-newsletter" type="submit">Confirm</button>
+                                                        </div>}
+
+                                                </div>
+                                            </form>
+                                        
+
+                        </div>
+                    </div>
+                </div>
+
+    
     
 
 </footer>
